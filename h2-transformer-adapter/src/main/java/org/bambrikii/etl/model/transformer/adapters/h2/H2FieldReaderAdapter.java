fe16ee0297d6c3b9d3450fc0b62a4c3032cecbaf
Alexander Arakelyan <alexander.arakelyan@gmail.com>
2020-01-08 15:45:13 -0500

    basic structure

---
+++ h2-transformer-adapter/src/main/java/org/bambrikii/etl/model/transformer/adapters/h2/H2FieldReaderAdapter.java
@@ -0,0 +1,20 @@
+
+import org.bambikii.etl.model.transformer.adapters.FieldReaderAdapter;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Alexander_Arakelian
+ * @param <T>
+ */
+public class H2FieldReaderAdapter<T> implements FieldReaderAdapter<T> {
+
+    public void readField(T obj, String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
---
+++ h2-transformer-adapter/src/main/java/org/bambrikii/etl/model/transformer/adapters/h2/H2FieldWriterAdapter.java
@@ -0,0 +1,19 @@
+
+import org.bambikii.etl.model.transformer.adapters.FieldWriterAdapter;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Alexander_Arakelian
+ */
+public class H2FieldWriterAdapter<T> implements FieldWriterAdapter<T> {
+
+    public void writeField(T obj, String fieldName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
